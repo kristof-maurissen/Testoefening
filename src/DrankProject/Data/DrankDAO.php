c167687e835840e204121dcad71378ca88fc43ac
kristof-maurissen <maurissen.kristof@gmail.com>
2015-12-06 19:04:35 +0100

    Update DrankDAO.php

--- src/DrankProject/Data/DrankDAO.php
+++ src/DrankProject/Data/DrankDAO.php
@@ -5,7 +5,8 @@ namespace DrankProject\Data;
 
 use DrankProject\Data\DBConfig;
 use DrankProject\Entities\Dranken;
-//use DrankProject\Exceptions\VoorraadLeegExcept;
+use DrankProject\Exceptions\VoorraadLeegExcept;
+
 use PDO;
 
 class DrankDAO {
@@ -24,15 +25,7 @@ class DrankDAO {
             return $lijst;       
     }
     
-    public function updateDrank($drank) {
-        $sql = "update dranken set dranknaam = :dranknaam, prijs = :prijs, voorraad = :voorraad, foto = :foto where id = :id";
-        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
-        $stmt = $dbh->prepare($sql);
-        $stmt->execute(array(":dranknaam" => $drank->getDrankNaam(), ":prijs" => $drank->getPrijs(), ":voorraad" => $drank->getVoorraad(), ":foto" => $drank->getFoto(), ":id" => $drank->getId()));
-        $dbh = null;
-    }
-    
-    public function getVooraadById($id) {
+    public function getDrankById($id) {
         $sql = "select id, dranknaam, prijs, voorraad from dranken where id = :id";
         $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
         
@@ -45,6 +38,20 @@ class DrankDAO {
         $dbh = null; 
         return $dranken; 
     }
+    public function updateDrank($id, $voorraad) {
+        $drank = $this->getDrankById($id);
+            if($drank->getVoorraad() <= 0 ) {
+                throw new VoorraadLeegExcept();
+            }
+            
+        $sql = "update dranken set voorraad = :voorraad where id = :id";
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(array( ":voorraad" => $voorraad, ":id" => $id));
+        $dbh = null;
+    }
+    
+    
     
 }
 
