364997c2d12a8b77f23269d0e5a0afa46e195ba4
kristof-maurissen <maurissen.kristof@gmail.com>
2015-12-06 19:01:53 +0100

    Update Bootstrap.php

--- Bootstrap.php
+++ Bootstrap.php
@@ -5,7 +5,7 @@ require_once("Doctrine/Common/ClassLoader.php");
 $classLoader = new ClassLoader("DrankProject", "src");
 $classLoader->register();
 
-/*require_once("Libraries/Twig/Autoloader.php"); 
+require_once("Libraries/Twig/Autoloader.php"); 
 Twig_Autoloader::register(); 
-$loader = new Twig_Loader_Filesystem("src/VDAB/MijnProject/Presentation"); 
-$twig = new Twig_Environment($loader);*/
+$loader = new Twig_Loader_Filesystem("src/DrankProject/Presentation"); 
+$twig = new Twig_Environment($loader);
